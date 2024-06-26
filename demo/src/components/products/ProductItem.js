5f5a20c0647348384008627c3447a15ee1014b04
cdufour <opusidea@gmail.com>
2024-06-26 08:58:07 +0200

    exo4

---
+++ demo/src/components/products/ProductItem.js
@@ -0,0 +1,16 @@
+import './ProductItem.css';
+export default function ProductItem({product}) {
+
+    const { name, price, stock, online } = product;
+    const onlineTxt = online ? 'Disponible en ligne uniquement' : 'Indisponible en ligne';
+    const style1 = { fontSize: '7pt' };
+
+    return (
+        <article>
+            <h3>{name}</h3>
+            <p className='product-price'>Prix: {price} EUR</p>
+            <p>Stock: {stock} unités - <span style={style1}>{onlineTxt}</span></p>
+            <hr />
+        </article>
+    )
+}
--- demo/src/components/products/ProductList.js
+++ demo/src/components/products/ProductList.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import ProductItem from './ProductItem';
+
+const API = 'http://localhost:4000/products';
 
 export default class ProductList extends React.Component {
+
+    constructor(props) {
+        super();
+        this.state = { products: [] };
+    }
+
+
+    componentDidMount() {
+        // requête ajax
+        fetch(API, { method: 'get' })
+            .then(res => res.json())
+            .then(products => this.setState({products}))
+    }
+
     render() {
-        return <p>...</p>
+        console.log('render');
+        return (
+            <div>
+                <h3>Products ({ this.state.products.length })</h3>
+                {
+                    this.state.products
+                        .map(product => <ProductItem key={product.id} product={product} />)
+                }
+            </div>
+        )
     }
 }
